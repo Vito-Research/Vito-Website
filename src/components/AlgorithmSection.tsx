f4fa91465f79459cf49e92f6b40109bfce2baaf9
Andreas Ink <andreasink17@gmail.com>
2022-05-29 15:53:36 -0400

    Apps and Algo Sections

--- src/components/AlgorithmSection.tsx
+++ src/components/AlgorithmSection.tsx
@@ -1,7 +1,15 @@
 import "./algorithmSection.scss";
+import Algo from "../res/Algo.svg";
 
 function AlgorithmSection() {
-  return <div className="section algorithm" id="algorithm" />;
+  return (
+    <div className="section algorithm" id="algorithm">
+      <h1 className="algorithm-header">Algorithm</h1>
+
+      <img alt="algorthim" className="algo" src={Algo} />
+      <caption className="algorithm-caption">Vito uses a deterministic finite state machine to detect higher than normal heart rate and post study respiration rate.  We look to expand on machine learning by using neural networks.  A deterministic finite state is a level system that has certain conditions to build on each previous data point.  One study by Stanford University found that a median of 4.3 days prior to visible symptom onset of Covid-19, their algorithm was able to detect Covid-19.</caption>
+    </div>
+  );
 }
 
 export default AlgorithmSection;
