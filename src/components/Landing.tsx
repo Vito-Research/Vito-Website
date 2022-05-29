7ab4d35c2f301e80b19a194560f49ad5869be09c
Ricky LeDew <adotout1@gmail.com>
2022-05-29 13:12:17 -0400

    Improved styles of landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -4,15 +4,13 @@ import Logo from "./Logo";
 
 function Landing() {
   return (
-    <div className="App">
-      <div className="textCenter">
-        <div className="textLeft">
-          <h1>Reduce The Loss Of Life Due to Infection</h1>
-          <h3>An app that detects infection in realtime</h3>
-          <button type="button" className="cta">
-            Learn More
-          </button>
-        </div>
+    <div className="section landing">
+      <div className="landing-text">
+        <h1>Reduce The Loss Of Life Due to Infection</h1>
+        <h3>An app that detects infection in realtime</h3>
+        <button type="button" className="cta">
+          Learn More
+        </button>
       </div>
       <Logo />
     </div>
