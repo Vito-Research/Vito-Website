27750f81e94fcff6d227767d17595c7977d206aa
Ricky LeDew <adotout1@gmail.com>
2022-05-29 15:06:25 -0400

    Merge other commit

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,15 +1,21 @@
 import "./aboutSection.scss";
-import Info from "../res/VitoMethod.svg"; 
+import Info from "../res/VitoMethod.svg";
+
 function AboutSection() {
-
-  return <div className="section about" id="about" > 
-  <div className="about-text">
-      <h1 className="header">Estimated 4% Reduction in Covid-19 Infections <sup>1</sup></h1>
-      <h3 className="header light">Vito encourages people to take precautions they deem necessary when possibly infected</h3>
-      <img src={Info}></img>
+  return (
+    <div className="section about" id="about">
+      <div className="about-text">
+        <h1 className="header">
+          Estimated 4% Reduction in Covid-19 Infections <sup>1</sup>
+        </h1>
+        <h3 className="header light">
+          Vito encourages people to take precautions they deem necessary when
+          possibly infected
+        </h3>
+        <img src={Info} alt="Info" />
+      </div>
     </div>
-</div>;
+  );
 }
-
 
 export default AboutSection;
