a772d07c4be5f63fd57fbd61a991b269e140bd29
Ricky LeDew <adotout1@gmail.com>
2022-05-31 12:31:14 -0400

    Fixed some overflow and redid navbar

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -17,16 +17,19 @@ function Landing() {
     });
   }, []);
   return (
-    <div className="section landing">
-      <div className="landing-text" style={{ opacity: landingTextOpacity }}>
-        <h1>Reduce The Loss Of Life Due to Infection</h1>
-        <h3>An app that detects infection in realtime</h3>
-        <button type="button" className="btn btn-cta">
-          Learn More
-        </button>
+    <>
+      <div className="gradient" />
+      <div className="section landing">
+        <div className="landing-text" style={{ opacity: landingTextOpacity }}>
+          <h1>Reduce The Loss Of Life Due to Infection</h1>
+          <h3>An app that detects infection in realtime</h3>
+          <button type="button" className="btn btn-cta">
+            Learn More
+          </button>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+    </>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,37 +2,31 @@ import "./navbar.scss";
 
 function Navbar() {
   return (
-    <div className="Navbar" id="Navbar">
-      <ul className="nav-list">
-        <li className="nav-brand">
-          <a href="/"> Vito </a>
-        </li>
+    <nav className="navbar" id="navbar">
+      <div className="nav-brand">
+        <a href="/"> Vito </a>
+      </div>
 
-        <li className="nav-item">
-          <button
-            style={{ margin: "5%", width: "20vw" }}
-            type="button"
-            className="btn btn-cta nav-btn"
-          >
-            Join The Community
-          </button>
-        </li>
-        <li className="nav-item">
-          <a href="bib.html">Algorithm</a>
-        </li>
-        <li className="nav-item">
-          <a href="consact.html">Apps</a>
-        </li>
-        <li className="nav-item">
-          <a href="sharkspecies.html">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="active" href="index.html">
-            Home
-          </a>
-        </li>
-      </ul>
-    </div>
+      <div className="nav-item">
+        <a className="active" href="index.html">
+          Home
+        </a>
+      </div>
+      <div className="nav-item">
+        <a href="sharkspecies.html">About</a>
+      </div>
+      <div className="nav-item">
+        <a href="consact.html">Apps</a>
+      </div>
+      <div className="nav-item">
+        <a href="bib.html">Algorithm</a>
+      </div>
+      <div className="nav-item">
+        <button type="button" className="btn btn-secondary">
+          Join The Community
+        </button>
+      </div>
+    </nav>
   );
 }
 
